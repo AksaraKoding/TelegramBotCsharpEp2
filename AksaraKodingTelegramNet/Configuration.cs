2870eadd2ee321db825c70a61322726ceba69b1f
Aksara Koding <74696150+AksaraKoding@users.noreply.github.com>
2020-11-19 14:11:33 +0700

    Update Configuration.cs

--- AksaraKodingTelegramNet/Configuration.cs
+++ AksaraKodingTelegramNet/Configuration.cs
@@ -8,6 +8,6 @@ namespace AksaraKodingTelegramNet
 {
     public static class Configuration
     {
-        public readonly static string BotToken = "1437681606:AAEhk5iXh9pFWJBfVohszuaosi5t3XrwVrg";
+        public readonly static string BotToken = "TOKEN";
     }
 }
